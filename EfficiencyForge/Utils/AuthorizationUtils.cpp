7b291c5fcee64d370eff1e112e149ac587f02b1c
matvey_makaro <matvey.makaro@gmail.com>
2023-12-01 13:39:06 +0300

    Save work.

--- EfficiencyForge/Utils/AuthorizationUtils.cpp
+++ EfficiencyForge/Utils/AuthorizationUtils.cpp
@@ -3,12 +3,12 @@
 
 bool IsLoginCorrect(const QString &login)
 {
-    QRegExp regExp("/^[a-z0-9_-]{3,16}$/");
+    QRegExp regExp("^[a-z0-9_-]{3,16}$");
     return regExp.exactMatch(login);
 }
 
 bool IsPasswordCorrect(const QString &password)
 {
-    QRegExp regExp("/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/");
+    QRegExp regExp("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$");
     return regExp.exactMatch(password);
 }
